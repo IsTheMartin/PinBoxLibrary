3a689634836817ac6c65ad5e75e952cc55b82443
Martin Ignacio Morales de Arco <isthemartin@gmail.com>
2019-08-10 17:43:17 -0500

    First time that files uploads. I inserted a new module for library with a file for colors.

---
+++ app/src/main/java/ismartin/com/pinboxapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package ismartin.com.pinboxapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
